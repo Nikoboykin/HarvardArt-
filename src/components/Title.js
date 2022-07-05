106436aa345bc14ebae7cd54365e9bb41b961e61
Niko <nikoboykin@gmail.com>
2022-07-04 23:21:29 -0400

    still trying to understand concept

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,12 +3,19 @@ export { Title } from './Title.js';
 
 //  * Create/export a component called Title which uses this static HTML as the template:
 //  * 
-  <div id="title">
-    <h1>
-      The Art Collector
-    </h1>
-    <h5>
-      Search the Harvard Art Museums' Private Collections
-    </h5>
-  </div>
+  // <div id="title">
+  //   <h1>
+  //     The Art Collector
+  //   </h1>
+  //   <h5>
+  //     Search the Harvard Art Museums' Private Collections
+  //   </h5>
+  // </div>
  
+  const Title = (props) => {
+    return (
+        <div>Title Placeholder</div>
+    )
+}
+
+export default Title; 
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-export { default as Feature } from './components/Feature';
-export { default as Loading } from './components/Loading';
-export { default as Preview } from './components/Preview';
-export { default as Search } from './components/Search';
-export { default as Title } from './components/Title';
+export { default as Feature } from './Feature';
+export { default as Loading } from './Loading';
+export { default as Preview } from './Preview';
+export { default as Search } from './Search';
+export { default as Title } from './Title';
 /**
  * The index.js file is used to re-export from our separate files, that way rather than write:
  * 
